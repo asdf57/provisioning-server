219592bf14e3dbe0a2a773aeafe38a780fd65b96
asdf57 <53836417+asdf57@users.noreply.github.com>
2025-02-16 00:49:37 -0500

    Add keyscan for git pub key

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,4 +11,5 @@ mkdir -p /root/.ssh
 echo "$SSH_KEY" > /root/.ssh/git_provisioning_key
 chmod 600 /root/.ssh/git_provisioning_key
 ssh-add /root/.ssh/git_provisioning_key
+ssh-keyscan -t rsa github.com >> /root/.ssh/known_hosts
 python /app/main.py
