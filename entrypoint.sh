99ab9c4419155c7f7779441f6d848fcce9546bea
asdf57 <53836417+asdf57@users.noreply.github.com>
2025-02-16 00:06:00 -0500

    Add handling for Docker secrets

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 eval "$(ssh-agent -s)" >/dev/null 2>&1
+
+if [[ -f /run/secrets/git_provisioning_key ]]; then
+    SSH_KEY=$(cat /run/secrets/git_provisioning_key)
+fi
+
 echo "$SSH_KEY" > /root/.ssh/git_prov_key
 chmod 600 /root/.ssh/git_prov_key
 chmod 600 /root/.ssh/config
